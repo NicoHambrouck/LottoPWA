9f5fef0444ee180d1556bc64e5c5e66f78c64f6b
NicoHambrouck <49762806+NicoHambrouck@users.noreply.github.com>
2026-03-04 11:12:36 +0100

    sdfsf

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const CACHE_NAME = "lotto-pwa-cache";
 
 const urlsToCache = [
@@ -21,28 +20,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-=======
-const CACHE_NAME = "lotto-pwa-cache";
-
-const urlsToCache = [
-  "/",
-  "/lotto.html",
-  "/lotto.css",
-  "/lotto.js",
-  "/manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
->>>>>>> df61a4ac0d1281770a4f1410135a71e9246fb138
 });
